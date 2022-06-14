f9e76cfc84ecfcb5dc7e30829d2b7b5bcec5e1d8
sundarbadagala <74392647+sundarthedevoloper@users.noreply.github.com>
2022-06-14 22:51:05 +0530

    recode

--- 03id.js
+++
@@ -1,1 +0,0 @@
-document.getElementById('main').textContent='hello wrold'
--- 05element.js
+++
@@ -1,3 +0,0 @@
-let content = document.createElement('div')
-content.textContent = 'createing new p element'
-document.body.appendChild(content)
