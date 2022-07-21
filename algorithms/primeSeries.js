daf126f54fa1ccea1c3307561241dd435d9b0255
sundarbadagala <74392647+sundarthedevoloper@users.noreply.github.com>
2022-07-21 16:58:56 +0530

    algorithms

--- algorithms/primeSeries.js
+++ algorithms/primeSeries.js
@@ -2,7 +2,8 @@ function primeSeries (n){
     let arr=[]
     for(let i=1; i<=n ; i++){
         let flag = true
-        for(let j=2; j<i; j++){
+        if(i%2 === 0 && i !== 2) flag = false
+        for(let j=3; j<i; j++, j++){
             if(i%j === 0){
                 flag = false
             }
