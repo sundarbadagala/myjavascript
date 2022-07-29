bde5b6cefbd064a21bbfb91506a07594611cc477
sundarbadagala <74392647+sundarthedevoloper@users.noreply.github.com>
2022-07-29 17:15:51 +0530

    data tasks

--- dataTasks/index.js
+++ dataTasks/index.js
@@ -1,3 +1,4 @@
-import { dummyData} from './data/dummy.js'
+import { complexDummyData} from './data/complexDummy.js'
+import {jsonData} from './data/json.js'
 import {objData1, objData2, objData3, arrData1, arrData2, arrData3} from './data/examples.js'
 import {isSameObjects, isDataAvailabe, updateData, addData, isSameArrays} from './helpers.js'
