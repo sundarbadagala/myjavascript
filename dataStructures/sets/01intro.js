949bd783d963f0f16d0c44b184fcfa17501fd9c1
sundarbadagala <74392647+sundarthedevoloper@users.noreply.github.com>
2022-08-27 13:09:42 +0530

    sets

--- dataStructures/sets/01intro.js
+++ dataStructures/sets/01intro.js
@@ -14,3 +14,12 @@
 //set.keys() – returns an iterable object for values,
 //set.values() – same as set.keys(), for compatibility with Map,
 //set.entries() – returns an iterable object for entries [value, value], exists for compatibility with Map.
+
+
+//-----------------------------------------
+
+//a set is a data structure that can hold a collection of values the values however must be unique
+//sets can contain a mix of different data types you can store strings booleans numbers or even objects all in the same set
+//sets are dynamically sized you don't have to declare the size of a set before creating it
+//sets do not maintain an insertion order an item inserted first does not necessarily mean it is the first element in the set
+//sets are iterables they can be used with a for off loop
