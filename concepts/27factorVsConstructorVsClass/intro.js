ea1d78eb9fe732d938ac0c2a3f94f21b137c2642
sundarbadagala <74392647+sundarthedevoloper@users.noreply.github.com>
2023-02-14 00:41:06 +0530

    difference between factory functions and constructor functions and classes

---
+++ concepts/27factorVsConstructorVsClass/intro.js
@@ -0,0 +1,1 @@
+//the general purpose of both is to write your object creation logic once and use either function to create multiple objects with any dynamic information they have passed in as parameters
