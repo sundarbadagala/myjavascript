a4e6a4a4054fbaa5648577d178473c84cfeef1a5
sundar <sundararao.b@narayanagroup.com>
2023-11-06 11:51:17 +0530

    promise

--- concepts/23promise/intro.js
+++ concepts/23promise/intro.js
@@ -1,12 +1,36 @@
-const promise = new Promise((resolve, reject)=>{
-    const isOk = true
-    if(isOk){
-        resolve('resolved')
-    }else{
-        reject('rejected')
-    }
-})
+// const promise = new Promise((resolve, reject)=>{
+//     const isOk = true
+//     if(isOk){
+//         resolve('resolved')
+//     }else{
+//         reject('rejected')
+//     }
+// })
 
-promise
-.then(res => console.log(res))
-.catch(err => console.log(err))
+// promise
+// .then(res => console.log(res))
+// .catch(err => console.log(err))
+
+const callback = () => {
+  return "hello";
+};
+
+const callback2 = (str) => {
+  return `${str} world`;
+};
+
+const vari = 10;
+
+const promise = (cb) =>
+  new Promise((resolve, reject) => {
+    if (typeof cb === "function") {
+      resolve(cb);
+    } else {
+      reject("callback is not function");
+    }
+  });
+
+promise(callback)
+  .then((res) => res())                         //hello
+  .then((res) => console.log(callback2(res)))   //hello world
+  .catch((err) => console.log(err));
